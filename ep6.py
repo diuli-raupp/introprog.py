5a5e57a554ccf93dc4e139be846a8d710ef021b4
diuli-raupp <137963003+diuli-raupp@users.noreply.github.com>
2023-07-02 21:35:15 -0300

    Create ep6.py

---
+++ ep6.py
@@ -0,0 +1,28 @@
+'''
+500g 
+400 s
+30 min 45 s
+-------------------------------
+30 min + 45 s = 1845 s
+
+1845 / 400 = 4,6125 = n
+
+ i = 0
+
+while i < n:
+   massa = 500/2
+   i += 1
+-------------------------------
+contador = 0
+
+	while massa > 1:
+	massa = 500/2
+	contador += 1
+
+contador * tempo = tempo '''
+
+import datetime
+
+massa = float(input())
+meia_vida = int(input())
+tempo = input().split(" ")
